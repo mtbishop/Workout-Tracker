322240cda236bc1b500512870d4f389e8ad28442
mtbishop <bishopmatthew76@gmail.com>
2021-04-18 21:47:51 -0500

    fixed express apply error

--- routes/index.js
+++
@@ -1,8 +0,0 @@
-const router = require('express')
-const apiRoutes = require('./apiRoutes')
-const htmlRoutes = require('./htmlRoutes')
-
-router.use(apiRoutes);
-router.use(htmlRoutes);
-
-module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const logger = require('morgan');
 
 const PORT = process.env.PORT || 3000;
 
@@ -8,8 +7,6 @@ const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(logger('dev'));
-
 app.use(express.static('public'));
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workout', {
