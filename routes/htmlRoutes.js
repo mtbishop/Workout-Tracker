a579681260d563d7c05c235b5f54a4bc33e3c864
mtbishop <bishopmatthew76@gmail.com>
2021-04-21 21:41:36 -0500

    fixed route paths

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,20 +1,18 @@
-const app = require('express');
 const path = require('path');
 
 module.exports = (app) => {
 
 app.get('/exercise',
   (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/exercise.html'));
+    res.sendFile(path.resolve('public/exercise.html'));
   });
 app.get('/stats',
   (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/stats.html'));
+    res.sendFile(path.resolve('public/stats.html'));
   });
 app.get('/',
   (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/index.html'));
+    res.sendFile(path.resolve('public/index.html'));
   });
 }
 
-module.exports = app;
